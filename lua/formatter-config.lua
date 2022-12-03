2c483c9bd33037900caa517fc184f2d2417a7b09
name <yes@localhost>
2022-12-03 21:23:25 +0100

    frontend formatters

--- lua/formatter-config.lua
+++ lua/formatter-config.lua
@@ -7,6 +7,15 @@ require("formatter").setup {
       require('formatter.filetypes.python').black,
       require('formatter.filetypes.python').isort,
     },
+    html = {
+      require('formatter.filetypes.html').prettier,
+    },
+    javascript = {
+      require('formatter.filetypes.javascript').prettier,
+    },
+    css = {
+      require('formatter.filetypes.css').prettier,
+    },
     ["*"] = {
       require("formatter.filetypes.any").remove_trailing_whitespace
     }
